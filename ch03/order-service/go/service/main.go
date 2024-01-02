23d51bfa2b9c4026d42b76fa6188b839f3f4e50c
ralph <ralphxwz@gmail.com>
2024-01-02 11:45:02 +0900

    bug fix chap03

--- ch03/order-service/go/service/main.go
+++ ch03/order-service/go/service/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/golang/protobuf/ptypes/wrappers"
+	"io"
+	"log"
+	"net"
+	pb "ordermgt/service/ecommerce"
+	"strings"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	/*"github.com/golang/protobuf/ptypes/wrappers"
-	"google.golang.org/grpc"*/
-	pb "ordermgt/service/ecommerce"
 	wrapper "github.com/golang/protobuf/ptypes/wrappers"
-	"io"
-	"log"
-	"net"
-	"strings"
 )
 
 const (
@@ -23,7 +21,7 @@ const (
 	orderBatchSize = 3
 )
 
-var orderMap = make(map[string]pb.Order)
+// var orderMap = make(map[string]pb.Order)
 
 type server struct {
 	orderMap map[string]*pb.Order
@@ -32,32 +30,30 @@ type server struct {
 // Simple RPC
 func (s *server) AddOrder(ctx context.Context, orderReq *pb.Order) (*wrapper.StringValue, error) {
 	log.Printf("Order Added. ID : %v", orderReq.Id)
-	orderMap[orderReq.Id] = *orderReq
+	s.orderMap[orderReq.Id] = orderReq
 	return &wrapper.StringValue{Value: "Order Added: " + orderReq.Id}, nil
 }
 
 // Simple RPC
 func (s *server) GetOrder(ctx context.Context, orderId *wrapper.StringValue) (*pb.Order, error) {
-	ord, exists := orderMap[orderId.Value]
+	ord, exists := s.orderMap[orderId.Value]
 	if exists {
-		return &ord, status.New(codes.OK, "").Err()
+		return ord, status.New(codes.OK, "").Err()
 	}
 
-	return nil, status.Errorf(codes.NotFound, "Order does not exist. : ", orderId)
-
-
+	return nil, status.Errorf(codes.NotFound, "Order does not exist. : %v", orderId)
 }
 
 // Server-side Streaming RPC
-func (s *server) SearchOrders(searchQuery *wrappers.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
+func (s *server) SearchOrders(searchQuery *wrapper.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
 
-	for key, order := range orderMap {
+	for key, order := range s.orderMap {
 		log.Print(key, order)
 		for _, itemStr := range order.Items {
 			log.Print(itemStr)
 			if strings.Contains(itemStr, searchQuery.Value) {
 				// Send the matching orders in a stream
-				err := stream.Send(&order)
+				err := stream.Send(order)
 				if err != nil {
 					return fmt.Errorf("error sending message to stream : %v", err)
 				}
@@ -84,7 +80,7 @@ func (s *server) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) erro
 			return err
 		}
 		// Update order
-		orderMap[order.Id] = *order
+		s.orderMap[order.Id] = order
 
 		log.Printf("Order ID : %s - %s", order.Id, "Updated")
 		ordersStr += order.Id + ", "
@@ -95,7 +91,7 @@ func (s *server) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) erro
 func (s *server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
 
 	batchMarker := 1
-	var combinedShipmentMap = make(map[string]pb.CombinedShipment)
+	var combinedShipmentMap = make(map[string]*pb.CombinedShipment)
 	for {
 		orderId, err := stream.Recv()
 		log.Printf("Reading Proc order : %s", orderId)
@@ -104,7 +100,7 @@ func (s *server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) er
 			// Send remaining shipments
 			log.Printf("EOF : %s", orderId)
 			for _, shipment := range combinedShipmentMap {
-				if err := stream.Send(&shipment); err != nil {
+				if err := stream.Send(shipment); err != nil {
 					return err
 				}
 			}
@@ -115,56 +111,82 @@ func (s *server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) er
 			return err
 		}
 
-		destination := orderMap[orderId.GetValue()].Destination
+		destination := s.orderMap[orderId.GetValue()].Destination
 		shipment, found := combinedShipmentMap[destination]
 
 		if found {
-			ord := orderMap[orderId.GetValue()]
-			shipment.OrdersList = append(shipment.OrdersList, &ord)
+			ord := s.orderMap[orderId.GetValue()]
+			shipment.OrdersList = append(shipment.OrdersList, ord)
 			combinedShipmentMap[destination] = shipment
 		} else {
-			comShip := pb.CombinedShipment{Id: "cmb - " + (orderMap[orderId.GetValue()].Destination), Status: "Processed!", }
-			ord := orderMap[orderId.GetValue()]
-			comShip.OrdersList = append(shipment.OrdersList, &ord)
+			comShip := &pb.CombinedShipment{Id: "cmb - " + (s.orderMap[orderId.GetValue()].Destination), Status: "Processed!"}
+			ord := s.orderMap[orderId.GetValue()]
+			comShip.OrdersList = []*pb.Order{ord}
 			combinedShipmentMap[destination] = comShip
 			log.Print(len(comShip.OrdersList), comShip.GetId())
 		}
 
 		if batchMarker == orderBatchSize {
 			for _, comb := range combinedShipmentMap {
-				log.Printf("Shipping : %v -> %v" , comb.Id, len(comb.OrdersList))
-				if err := stream.Send(&comb); err != nil {
+				log.Printf("Shipping : %v -> %v", comb.Id, len(comb.OrdersList))
+				if err := stream.Send(comb); err != nil {
 					return err
 				}
 			}
 			batchMarker = 0
-			combinedShipmentMap = make(map[string]pb.CombinedShipment)
+			combinedShipmentMap = make(map[string]*pb.CombinedShipment)
 		} else {
 			batchMarker++
 		}
 	}
 }
 
+func initSampleData(s *server) {
+	s.orderMap = make(map[string]*pb.Order)
+	s.orderMap["102"] = &pb.Order{
+		Id:          "102",
+		Items:       []string{"Google Pixel 3A", "Mac Book Pro"},
+		Destination: "Mountain View, CA",
+		Price:       1800.00}
+
+	s.orderMap["103"] = &pb.Order{
+		Id:          "103",
+		Items:       []string{"Apple Watch S4"},
+		Destination: "San Jose, CA",
+		Price:       400.00}
+
+	s.orderMap["104"] = &pb.Order{
+		Id:          "104",
+		Items:       []string{"Google Home Mini", "Google Nest Hub"},
+		Destination: "Mountain View, CA",
+		Price:       400.00}
+
+	s.orderMap["105"] = &pb.Order{
+		Id:          "105",
+		Items:       []string{"Amazon Echo"},
+		Destination: "San Jose, CA",
+		Price:       30.00}
+
+	s.orderMap["106"] = &pb.Order{
+		Id:          "106",
+		Items:       []string{"Amazon Echo", "Apple iPhone XS"},
+		Destination: "Mountain View, CA",
+		Price:       300.00}
+}
 
 func main() {
-	initSampleData()
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterOrderManagementServer(s, &server{})
+	srv := &server{}
+	initSampleData(srv)
+
+	pb.RegisterOrderManagementServer(s, srv)
 	// Register reflection service on gRPC server.
 	// reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-}
-
-func initSampleData() {
-	orderMap["102"] = pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Mac Book Pro"}, Destination: "Mountain View, CA", Price: 1800.00}
-	orderMap["103"] = pb.Order{Id: "103", Items: []string{"Apple Watch S4"}, Destination: "San Jose, CA", Price: 400.00}
-	orderMap["104"] = pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub"}, Destination: "Mountain View, CA", Price: 400.00}
-	orderMap["105"] = pb.Order{Id: "105", Items: []string{"Amazon Echo"}, Destination: "San Jose, CA", Price: 30.00}
-	orderMap["106"] = pb.Order{Id: "106", Items: []string{"Amazon Echo", "Apple iPhone XS"}, Destination: "Mountain View, CA", Price: 300.00}
 }
