28154859fb9ff808fbf82adc430abe7b9e020dbd
ralph <ralphxwz@gmail.com>
2024-07-27 21:24:20 +0900

    update

--- ch05/metadata/order-service/go/client/main.go
+++ ch05/metadata/order-service/go/client/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
+	pb "productinfo/client/ecommerce"
+	"time"
+
 	wrapper "github.com/golang/protobuf/ptypes/wrappers"
-	pb "github.com/grpc-up-and-running/samples/ch05/interceptors/order-service/go/order-service-gen"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
-	"io"
-	"log"
-	"time"
 )
 
 const (
@@ -29,7 +30,6 @@ func main() {
 	// originalCtx, cancel := context.WithTimeout(context.Background(), time.Second * 5)
 	// defer cancel()
 
-
 	// ****** Metadata : Creation *****
 
 	md := metadata.Pairs(
@@ -40,13 +40,11 @@ func main() {
 
 	ctxA := metadata.AppendToOutgoingContext(mdCtx, "k1", "v1", "k1", "v2", "k2", "v3")
 
-
 	// RPC using the context with new metadata.
 	var header, trailer metadata.MD
 
-
 	// RPC: Add Order
-	order1 := pb.Order{Id: "101", Items:[]string{"iPhone XS", "Mac Book Pro"}, Destination:"San Jose, CA", Price:2300.00}
+	order1 := pb.Order{Id: "101", Items: []string{"iPhone XS", "Mac Book Pro"}, Destination: "San Jose, CA", Price: 2300.00}
 	res, _ := client.AddOrder(ctxA, &order1, grpc.Header(&header), grpc.Trailer(&trailer))
 
 	log.Print("AddOrder Response -> ", res.Value)
@@ -69,7 +67,6 @@ func main() {
 		log.Fatal("location expected but doesn't exist in header")
 	}
 
-
 	// Search Order
 	searchStream, _ := client.SearchOrders(ctxA, &wrapper.StringValue{Value: "Google"})
 	for {
@@ -84,58 +81,17 @@ func main() {
 		}
 	}
 
-
 	// Update Orders
-	updOrder1 := pb.Order{Id: "102", Items:[]string{"Google Pixel 3A", "Google Pixel Book"}, Destination:"Mountain View, CA", Price:1100.00}
-	updOrder2 := pb.Order{Id: "103", Items:[]string{"Apple Watch S4", "Mac Book Pro", "iPad Pro"}, Destination:"San Jose, CA", Price:2800.00}
-	updOrder3 := pb.Order{Id: "104", Items:[]string{"Google Home Mini", "Google Nest Hub", "iPad Mini"}, Destination:"Mountain View, CA", Price:2200.00}
+	updOrder1 := pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Google Pixel Book"}, Destination: "Mountain View, CA", Price: 1100.00}
+	updOrder2 := pb.Order{Id: "103", Items: []string{"Apple Watch S4", "Mac Book Pro", "iPad Pro"}, Destination: "San Jose, CA", Price: 2800.00}
+	updOrder3 := pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub", "iPad Mini"}, Destination: "Mountain View, CA", Price: 2200.00}
 
 	updateStream, _ := client.UpdateOrders(mdCtx)
-
 
 	_ = updateStream.Send(&updOrder1)
 	_ = updateStream.Send(&updOrder2)
 	_ = updateStream.Send(&updOrder3)
 
-
 	updateRes, _ := updateStream.CloseAndRecv()
 	log.Printf("Update Orders Res : ", updateRes)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
