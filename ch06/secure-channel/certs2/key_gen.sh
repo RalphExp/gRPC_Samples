59f2f357edb5771479cdb87b7f6ca0c994fc2a36
ralph <ralphxwz@gmail.com>
2024-08-02 00:55:09 +0900

    update

--- ch06/secure-channel/certs2/key_gen.sh
+++ ch06/secure-channel/certs2/key_gen.sh
@@ -1,7 +1,9 @@
+# Generate private RSA key
 if [ ! -e server.key ]; then
 	openssl genrsa -out server.key 2048
 fi
 
+# self-signed certificate
 openssl req -new -x509 -sha256 -key server.key \
         -addext "subjectAltName = DNS:localhost" -out server.crt -days 3650
 
